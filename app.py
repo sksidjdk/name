91085212b098432218fa335c5ea1f028b175c67a
sksidjdk <m1677880993@qq.com>
2025-12-02 15:28:52 +0800

    Add HTTP server handler to respond with greeting
    Implement a basic HTTP server that responds with a greeting.

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print(hello world)
+from http.server import BaseHTTPRequestHandler
+
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        # HTTP 状态码 200 OK
+        self.send_response(200)
+        # 返回纯文本
+        self.send_header("Content-type", "text/plain; charset=utf-8")
+        self.end_headers()
+        # 实际返回内容
+        message = "Hello, World from Python on Vercel!"
+        self.wfile.write(message.encode("utf-8"))
